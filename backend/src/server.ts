26880434b679d320f38ef9d37a96d62c7ed3866a
Rhuan Oliveira <rhuan.dev@gmail.com>
2020-06-05 23:34:38 -0300

    feat: add cors and items images

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import path from 'path';
 import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 
