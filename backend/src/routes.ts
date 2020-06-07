1826300e6b7b79fce3adf77ca45b6326b5f58b90
Rhuan Oliveira <rhuan.dev@gmail.com>
2020-06-07 12:13:35 -0300

    feat: add image upload route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,7 +3,12 @@ import express from 'express';
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
 
+import multer from 'multer';
+import multerConfig from './config/multer';
+import { celebrate, Joi } from 'celebrate';
+
 const routes = express.Router();
+const upload = multer(multerConfig);
 
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
@@ -13,5 +18,25 @@ routes.get('/items', itemsController.index);
 routes.post('/points', pointsController.create);
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
+
+routes.post(
+  '/points',
+  upload.single('image'),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required(),
+      uf: Joi.string().required().max(2),
+      city: Joi.string().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      items: Joi.string().required(),
+    })
+  }, {
+    abortEarly: false,
+  }),
+  pointsController.create
+);
 
 export default routes;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import routes from './routes';
 import cors from 'cors';
+import { errors } from 'celebrate';
 
 const app = express();
 
@@ -12,5 +13,5 @@ app.use(express.json());
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-
+app.use(errors());
 app.listen(3333);
